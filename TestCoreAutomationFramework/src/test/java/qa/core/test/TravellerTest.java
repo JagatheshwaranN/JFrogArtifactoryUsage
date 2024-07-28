2a611a0432927d8f6c63dfcb2545e9b9b98ce7b4
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-07-28 10:53:51 +0530

    Test Full Code Commit

---
+++ TestCoreAutomationFramework/src/test/java/qa/core/test/TravellerTest.java
@@ -0,0 +1,52 @@
+package qa.core.test;
+
+import core.bundle.pageObject.TravelHomePage;
+import core.bundle.util.JsonReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TravellerTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        this.driver = new ChromeDriver();
+        this.driver.manage().window().maximize();
+    }
+
+    @Test(dataProvider = "getData")
+    public void flightTest(HashMap<String, String> travelDetail) {
+        TravelHomePage travelHomePage = new TravelHomePage(driver);
+        travelHomePage.navigateTo();
+        travelHomePage.getTopNavigation().searchFlight();
+        travelHomePage.getFooterNavigation().searchFlight();
+        travelHomePage.getTopNavigation().getLinksCount();
+        travelHomePage.getFooterNavigation().getLinksCount();
+        travelHomePage.setFlightBookingType(travelDetail.get("travelType"));
+        travelHomePage.inquireAvail(travelDetail);
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        String jsonFilePath = "src/test/resources/dataFile/travelDetail.json";
+        List<HashMap<String, String>> travelDetailList = JsonReader.getJsonData(jsonFilePath);
+        return new Object[][]{
+                {travelDetailList.get(0)},
+                {travelDetailList.get(1)}
+        };
+    }
+
+    @AfterTest
+    public void tearDown() {
+        this.driver.quit();
+    }
+
+}
